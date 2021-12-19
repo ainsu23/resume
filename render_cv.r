f9c648976f1e25383976b1d12c18e6dd47238b1f
Andres Felipe Insuasty Chamorro <afinsuasty@hotmail.com>
2021-12-19 17:31:38 -0500

    feat: Cambia index.html

--- render_cv.r
+++ render_cv.r
@@ -8,14 +8,14 @@
 # Knit the HTML version
 rmarkdown::render("cv.rmd",
                   params = list(pdf_mode = FALSE),
-                  output_file = "render.html")
+                  output_file = "index.html")
 
 # Knit the PDF version to temporary html location
 # tmp_html_cv_loc <- fs::file_temp(ext = ".html")
 # rmarkdown::render("cv.rmd",
 #                   params = list(pdf_mode = TRUE),
 #                   output_file = tmp_html_cv_loc)
-# 
+#
 # Convert to PDF using Pagedown
 # pagedown::chrome_print(input = tmp_html_cv_loc,
 #                        output = "cv_AndrésInsuasty.pdf")
